3475947bd493497c823e58a3049f4fc817892697
kuqmua <serezhashmakov@outlook.com>
2022-11-09 21:57:58 +0300

    working prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,132 @@
-#[proc_macro_derive(ProcMacroExampleName)]
-pub fn derive_(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(CompileTimeGitInfo)]
+pub fn derive_compile_time_git_info(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    //it only supported for enums without values
     let ast: syn::DeriveInput =
-        syn::parse(input).expect("ProcMacroExampleName syn::parse(input) failed");
+        syn::parse(input).expect("CompileTimeGitInfo syn::parse(input) failed");
     let ident = &ast.ident;
-    match ast.data {
-        syn::Data::Union(_) => panic!("ProcMacroExampleName does not work on union!"),
-        syn::Data::Struct(_) => panic!("ProcMacroExampleName does not work on structs!"),
-        syn::Data::Enum(_) => panic!("ProcMacroExampleName does not work on enums!"),
-    }
-    let gen = quote::quote! {};
+    let commit_id_token_stream = format!("\"kekw\"")
+        .parse::<proc_macro2::TokenStream>()
+        .expect("path parse failed");
+    //
+    // let path: String;
+    //     let git_folder_name = ".git";
+    //     let first_guess = format!("../{}{}/", repo_git_path, git_folder_name); //maybe here some error?
+    //     if Path::new(&first_guess).is_dir() {
+    //         path = first_guess; //for docker image or run not as tufa_project repo, as git clone tufa_server
+    //     } else if Path::new(&format!(
+    //         "../{}{}/",
+    //         GIT_PATH_FROM_SUBMODULE, git_folder_name
+    //     ))
+    //     .is_dir()
+    //     {
+    //         path = format!(
+    //             "../{}{}/modules/src/{}/",
+    //             GIT_PATH_FROM_SUBMODULE, git_folder_name, repo_name
+    //         );
+    //     } else {
+    //         panic!("error: no .git folder inside current and parent dir(this message should be displayed only on compile time)")
+    //     }
+    //     let full_path = &format!("{}{}", path, "logs/HEAD");
+    //     let file = File::open(Path::new(full_path))
+    //         .unwrap_or_else(|e| panic!("cannot open HEAD file, error: \"{}\"", e));
+    //     let mut buf_reader = BufReader::new(file);
+    //     let mut git_logs_head_content = String::new();
+    //     buf_reader
+    //         .read_to_string(&mut git_logs_head_content)
+    //         .unwrap_or_else(|e| panic!("cannot read to string from HEAD file, error: \"{}\"", e));
+    //     let from_handle = "from ";
+    //     let from_handle_index = git_logs_head_content
+    //         .find(from_handle)
+    //         .unwrap_or_else(|| panic!("no \"{}\" inside git_logs_head_content", from_handle));
+    //     let git_extenstion_name = ".git";
+    //     let dot_git_index = git_logs_head_content
+    //         .find(git_extenstion_name)
+    //         .unwrap_or_else(|| {
+    //             panic!(
+    //                 "no \"{}\" inside git_logs_head_content",
+    //                 git_extenstion_name
+    //             )
+    //         });
+    //     let repo_link = git_logs_head_content
+    //         .get(from_handle_index + from_handle.len()..dot_git_index)
+    //         .unwrap_or_else(|| panic!("failed to get slice from git_logs_head_content"))
+    //         .to_string();
+    //     let head_file_lines: Vec<&str> = git_logs_head_content.lines().collect::<Vec<&str>>();
+    //     let last_head_file_line = head_file_lines
+    //         .last()
+    //         .expect("no last element inside git head file lines");
+    //     let line_parts: Vec<&str> = last_head_file_line.split(' ').collect();
+    //     let commit_id = line_parts
+    //         .get(1)
+    //         .unwrap_or_else(|| panic!("failed to get 1 element from line_parts as commit_id"))
+    //         .to_string();
+    //     let author = line_parts
+    //         .get(2)
+    //         .unwrap_or_else(|| panic!("failed to get 2 element from line_parts as author"))
+    //         .to_string();
+    //     let unhandled_author_email = line_parts
+    //         .get(3)
+    //         .unwrap_or_else(|| {
+    //             panic!("failed to get 3 element from line_parts as slice for author_email")
+    //         })
+    //         .to_string();
+    //     let author_email = unhandled_author_email
+    //         .get(1..unhandled_author_email.len() - 1)
+    //         .unwrap_or_else(|| panic!("failed to get slice from line_parts as author_email"))
+    //         .to_string();
+    //     let commit_unix_time = line_parts
+    //         .get(4)
+    //         .unwrap_or_else(|| {
+    //             panic!("failed to get 4 element from line_parts as commit_unix_time")
+    //         })
+    //         .to_string();
+    //     let commit_unix_time_index =
+    //         last_head_file_line
+    //             .find(&commit_unix_time)
+    //             .unwrap_or_else(|| {
+    //                 panic!(
+    //                     "cannot find \"{}\" for the second time inside {}",
+    //                     commit_unix_time, git_logs_head_content
+    //                 )
+    //             });
+    //     let part_after_commit_unix_time = last_head_file_line
+    //         .get(commit_unix_time_index + commit_unix_time.len() + 1..)
+    //         .unwrap_or_else(|| {
+    //             panic!(
+    //                 "failed to get slice from last_head_file_line as part_after_commit_unix_time"
+    //             )
+    //         })
+    //         .to_string();
+    //     let backslash_t = "\t";
+    //     let backslash_t_index = part_after_commit_unix_time
+    //         .find(backslash_t)
+    //         .unwrap_or_else(|| {
+    //             panic!(
+    //                 "no \"{}\" inside \"{}\"",
+    //                 backslash_t, part_after_commit_unix_time
+    //             )
+    //         });
+    //     let message = part_after_commit_unix_time
+    //         .get(backslash_t_index + 1..)
+    //         .unwrap_or_else(|| {
+    //             panic!("failed to get slice from part_after_commit_unix_time as message")
+    //         })
+    //         .to_string();
+    //     let timezone = part_after_commit_unix_time
+    //         .get(..backslash_t_index)
+    //         .unwrap_or_else(|| {
+    //             panic!("failed to get slice from part_after_commit_unix_time as timezone")
+    //         })
+    //         .to_string();
+    //
+    let gen = quote::quote! {
+        impl Kekw<'_> {
+            pub fn get_git_commit_info<'a>(repo_git_path: &'a str, repo_name: &'a str) -> Kekw<'a> {
+                Kekw {
+                    commit_id: #commit_id_token_stream
+                }
+            }
+        }
+    };
     gen.into()
 }
