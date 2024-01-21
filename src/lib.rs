96700a041736f1db90f7003306d7c5bb2e107441
kuqmua <serezhashmakov@outlook.com>
2024-01-21 07:33:53 +0300

    panic location move to proc_macro_common

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn compile_time_git_info_tufa_common(
 }
 
 fn get_git_info(repo_name: &str, path_to_git_into_start_source: &str) -> proc_macro::TokenStream {
-    proc_macro_helpers::panic_location::panic_location();
+    proc_macro_common::panic_location::panic_location();
     let path_to_git_modules = format!("../.git/modules/src/{repo_name}/");
     let path = if std::path::Path::new(&path_to_git_modules).is_dir() {
         path_to_git_modules
@@ -153,7 +153,7 @@ fn get_git_info(repo_name: &str, path_to_git_into_start_source: &str) -> proc_ma
 pub fn compile_time_project_git_info(
     _input_token_stream: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    proc_macro_helpers::panic_location::panic_location();
+    proc_macro_common::panic_location::panic_location();
     let path_to_git_modules = "../.git";
     let path = if std::path::Path::new(&path_to_git_modules).is_dir() {
         path_to_git_modules
